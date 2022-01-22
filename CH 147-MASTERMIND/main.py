d42fabb87810e06c74d4cfb83379c39b77a2ac8c
symonkipkemei <symonkipkemei@gmail.com>
2022-01-22 21:54:13 +0300

    The mastermind logic
    the location of sub-programs is crucial to how the program runs.

--- CH 147-MASTERMIND/main.py
+++ CH 147-MASTERMIND/main.py
@@ -3,9 +3,31 @@ from sub_programs import *
 
 def main():
     myList = listColors()
-    usercolors = userColors(myList)
     compcolors = compColors(myList)
-    checker(compcolors, usercolors)
+    attempts = 0
+    correct_position = 0
+
+    print("\nThe computer has selected 4 colors already\n"
+          "Let's see if you can guess them correctly\n"
+          "all the best!")
+
+    while correct_position != 4:
+        usercolors_new = userColors(myList)
+        correct_position = checker(compcolors, usercolors_new)
+        attempts += 1
+
+    if attempts > 10:
+        print(f"Bro, You have attempted {attempts} times.\n"
+              f"you need to go to kindergarten, you too slow ")
+
+    elif attempts < 5:
+        print(f"Bro, You have attempted {attempts} times.\n"
+              f"you seem to have luck and charm up your sleeves! ")
+
+    else:
+        print(f"Bro, You have attempted {attempts} times.\n"
+              f"This is great! You have some supper potential! ")
 
 
 main()
+
